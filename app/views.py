f46420200c62e7c73c867973244f17a212ead55f
ookuboc5399 <ankle0614@gmail.com>
2021-04-17 12:57:43 +0900

    0417

--- app/views.py
+++ app/views.py
@@ -9,3 +9,10 @@ class IndexView(View):
         return render(request, 'app/index.html', {
             'post_data': post_data,
         })
+
+class PostDetailView(View):
+    def get(self, request, *args, **kwargs):
+        post_data = Post.objects.get(id=self.kwargs['pk'])
+        return render(request, 'app/post_detail.html', {
+            'post_data': post_data
+        })
